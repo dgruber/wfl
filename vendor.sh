b034fa62d9f0d0deb7c7eb7c402f4c9bee200da1
Daniel Gruber <dgruber@users.noreply.github.com>
2017-12-29 19:57:50 +0100

    Update vendor.sh

--- vendor.sh
+++ vendor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf vendor/ && govendor init && govendor add +e +x github.com/dgruber/drmaa2interface
+rm -rf vendor/ && govendor init && govendor add +e
 rm -rf vendor/github.com/dgruber/drmaa2interface
 
