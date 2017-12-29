3697356d7f1a21e3110d70d56eb1b4429f41f1b7
Daniel Gruber <mail@danielgruber.com>
2017-12-29 21:08:10 +0100

    iterator test

--- template_iterators_test.go
+++ template_iterators_test.go
@@ -7,18 +7,18 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"time"
 )
 
 var _ = Describe("TemplateIterators", func() {
 
-	Context("Set template's environment variable", func() {
+	Context("NewEnvSequenceIterator", func() {
 
 		jt := drmaa2interface.JobTemplate{}
 
 		It("should set the environment variable if not set", func() {
-
 			iter := wfl.NewEnvSequenceIterator("test", 100, 2)
-
 			newJt := iter(jt)
 
 			Ω(jt.JobEnvironment).Should(BeNil())
@@ -26,17 +26,14 @@ var _ = Describe("TemplateIterators", func() {
 			Ω(newJt.JobEnvironment["test"]).Should(Equal("100"))
 
 			tmpl := wfl.NewTemplate(jt).AddIterator("test", iter)
-
 			newJt = tmpl.Next()
 
 			Ω(newJt.JobEnvironment).ShouldNot(BeNil())
 			Ω(newJt.JobEnvironment["test"]).Should(Equal("102"))
-
 			Ω(jt.JobEnvironment).Should(BeNil())
 		})
 
 		It("should override the environment variable if set", func() {
-
 			iter := wfl.NewEnvSequenceIterator("test", 100, 2)
 
 			jt.JobEnvironment = make(map[string]string, 1)
@@ -45,7 +42,6 @@ var _ = Describe("TemplateIterators", func() {
 			newJt := iter(jt)
 
 			Ω(jt.JobEnvironment).ShouldNot(BeNil())
-
 			Ω(newJt.JobEnvironment).ShouldNot(BeNil())
 			Ω(newJt.JobEnvironment["test"]).Should(Equal("100"))
 
@@ -54,10 +50,24 @@ var _ = Describe("TemplateIterators", func() {
 
 			Ω(newJt.JobEnvironment).ShouldNot(BeNil())
 			Ω(newJt.JobEnvironment["test"]).Should(Equal("102"))
-
 			Ω(jt.JobEnvironment).ShouldNot(BeNil())
 		})
 
+	})
+
+	Context("NewTimeIterator", func() {
+
+		jt := drmaa2interface.JobTemplate{}
+
+		It("should return a new job template after the given duration", func() {
+			iter := wfl.NewTimeIterator(time.Millisecond * 25)
+			Ω(iter).ShouldNot(BeNil())
+			tmpl := wfl.NewTemplate(jt).AddIterator("timer", iter)
+			now := time.Now()
+			tmpl.Next()
+			tmpl.Next()
+			Ω(time.Now()).Should(BeTemporally(">=", now.Add(time.Millisecond*50)))
+		})
 	})
 
 })
